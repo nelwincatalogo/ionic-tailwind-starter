ad6b70bd591b4576afdbb582914d18803d1d35ab
Nelwin Catalogo <catalogonelwin@gmail.com>
2022-04-30 19:49:44 +0800

    ✨ implement tailwind

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,31 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle className="text-blue-500">Blank</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large" className="text-blue-500">
+              Blank
+            </IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+
+        <main className="bg-gray-200 h-full">
+          <h1 className="text-green-500 font-bold text-2xl">Hello World</h1>
+        </main>
       </IonContent>
     </IonPage>
   );
